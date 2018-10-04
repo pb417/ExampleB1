f57d2b6c5d18d9b5770fa21a98230437dc62642d
pb417 <42356293+pb417@users.noreply.github.com>
2018-10-04 10:30:56 -0400

    Add files via upload

---
+++ CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paula/Development/Geant4B1/exampleB1.cc" "/home/paula/Development/Geant4B1/CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "/home/paula/Development/Geant4B1/src/B1ActionInitialization.cc" "/home/paula/Development/Geant4B1/CMakeFiles/exampleB1.dir/src/B1ActionInitialization.cc.o"
+  "/home/paula/Development/Geant4B1/src/B1DetectorConstruction.cc" "/home/paula/Development/Geant4B1/CMakeFiles/exampleB1.dir/src/B1DetectorConstruction.cc.o"
+  "/home/paula/Development/Geant4B1/src/B1EventAction.cc" "/home/paula/Development/Geant4B1/CMakeFiles/exampleB1.dir/src/B1EventAction.cc.o"
+  "/home/paula/Development/Geant4B1/src/B1PrimaryGeneratorAction.cc" "/home/paula/Development/Geant4B1/CMakeFiles/exampleB1.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "/home/paula/Development/Geant4B1/src/B1RunAction.cc" "/home/paula/Development/Geant4B1/CMakeFiles/exampleB1.dir/src/B1RunAction.cc.o"
+  "/home/paula/Development/Geant4B1/src/B1SteppingAction.cc" "/home/paula/Development/Geant4B1/CMakeFiles/exampleB1.dir/src/B1SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/paula/Development/geant4.10.04.p02/include/Geant4"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
